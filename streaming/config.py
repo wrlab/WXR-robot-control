dc014e03787a765b44f16f76a34a24dfc1fb877c
Sehyeon Park <elan1537@gmail.com>
2023-12-07 14:25:25 +0900

    RTDE를 사용하여 UR5의 관절값을 socket stream으로 받음

--- streaming/config.py
+++
@@ -1,5 +0,0 @@
-cfg = {"HOST": "localhost", "PORT": 4070}
-
-from collections import namedtuple
-
-Config = namedtuple("Config", cfg.keys())(*cfg.values())
--- streaming/server.py
+++ streaming/server.py
@@ -1,31 +1,85 @@
-from robodk.robolink import Robolink
-from config import Config
-import socket
-import websockets
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.websockets import WebSocketDisconnect
+
 import asyncio
+import rtde.rtde as rtde
+import rtde.rtde_config as rtde_config
+import math
+import json
 
 
-if __name__ == "__main__":
-    RDK = Robolink()
-    robot = RDK.Item("UR5")
+app = FastAPI()
+connected = set()
 
-    is_active = False
-    active_count = 0
+# HOST = "169.254.16.195"
+HOST = "192.168.1.51"
+PORT = 30004
+FREQ = 125
 
-    async def handler(websocket):
-        global is_active
+origins = ["http://localhost:8000", "https://localhost", "http://localhost"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/{workspace_id}")
+async def main(workspace_id: int):
+    global conn
+    print("initialize: ", workspace_id)
+
+    conn = rtde.RTDE(HOST, PORT)
+    conn.connect()
+
+    return {"message": "Hello World"}
+
+
+@app.websocket("/stream/{workspace_id}")
+async def stream(websocket: WebSocket, workspace_id: int):
+    global conn
+    await websocket.accept()
+
+    conf = rtde_config.ConfigFile("record_configuration.xml")
+    output_names, output_types = conf.get_recipe("out")
+
+    try:
         while True:
-            if robot.Busy():
-                data = str(robot.Joints().list())
-                formatted_data = f"{len(data):05}|{data}"
-                print("Busy", formatted_data)
-                is_active = False
-                await websocket.send(formatted_data)
-            else:
-                if not is_active:
-                    print("Stop")
-                    is_active = True
+            if not conn.send_output_setup(output_names, output_types, frequency=FREQ):
+                print("Unable to configure output")
+                exit()
 
-    server = websockets.serve(handler, Config.HOST, Config.PORT)
-    asyncio.get_event_loop().run_until_complete(server)
-    asyncio.get_event_loop().run_forever()
+            if not conn.send_start():
+                print("Unable to start synchronization")
+                exit()
+
+            state = conn.receive()
+            joints = state.actual_q
+
+            await websocket.send_text(
+                json.dumps(
+                    {
+                        "len": len(joints),
+                        "timestamp": state.timestamp,
+                        "q1": joints[0] * 180 / math.pi,
+                        "q2": joints[1] * 180 / math.pi,
+                        "q3": joints[2] * 180 / math.pi,
+                        "q4": joints[3] * 180 / math.pi,
+                        "q5": joints[4] * 180 / math.pi,
+                        "q6": joints[5] * 180 / math.pi,
+                    }
+                )
+            )
+
+    except rtde.RTDEException:
+        conn.send_pause()
+        conn.disconnect()
+
+    except WebSocketDisconnect:
+        conn.send_pause()
+        print("Connection closed from", workspace_id)
+        await websocket.close()
