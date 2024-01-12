65bf3da92801971915959ec9b160b1150e79d9b6
Jaehong Yoo <143786746+galy96@users.noreply.github.com>
2024-01-12 18:30:43 +0900

    robodk boundingbox 설정
    가동범위를 넘어서 ik를 풀수 없는경우 WXR 서버에 변수 전달하는 부분 구현

--- robodk-kuka/robodk_kuka_server.py
+++ robodk-kuka/robodk_kuka_server.py
@@ -12,5 +12,5 @@ reference = RDK.Item('Baseline')
 
 # 웹소켓 통신 모듈 인스턴스 생성 및 서버 시작
 if __name__ == "__main__":
-    ws_comm = WebSocketCommunication(Config.HOST, Config.PORT, robot, tool, turntable)
+    ws_comm = WebSocketCommunication(Config.HOST, Config.PORT, robot, tool, turntable, RDK)
     ws_comm.start_server()
--- robodk-kuka/robodk_webSocket.py
+++ robodk-kuka/robodk_webSocket.py
@@ -8,10 +8,11 @@ import threading
 
 socket_lock = threading.Lock()
 
+
 def rad2degree(rad):
     return rad * 180 / math.pi
 class WebSocketCommunication:
-    def __init__(self, host, port, robot, tool, turntable):
+    def __init__(self, host, port, robot, tool, turntable, rdk):
         self.host = host
         self.port = port
         self.robot = robot
@@ -20,37 +21,51 @@ class WebSocketCommunication:
         self.previous_joints = None
         self.previous_tables = None
         self.on_table = False
+        self.rdk = rdk
 
         # IK 계산용 thread 생성
         self.position = None # 위치 데이터 초기화
         self.rotation = None # 회전 데이터 초기화
+        self.reachable = True # IK솔루션 있을 경우 초기화
         self.ik_results = None
         self.ik_thread = threading.Thread(target=self.ik_cal_thread)
         self.ik_thread.start()
         print("webSocket Start!")
 
+
     def ik_cal_thread(self):
         while True:
             if self.position and self.rotation:
-                # print("ik_cal Working!!")
+                #print("ik_cal Working!!")
                 local_pose = KUKA_2_Pose([1000 * self.position['x'], -1000 * self.position['z'], 1000 * self.position['y'], self.rotation['x'], self.rotation['y'], self.rotation['z']])
                 all_solutions = self.robot.SolveIK_All(local_pose, self.tool)
-                for j in all_solutions:
-                    conf_RLF = self.robot.JointsConfig(j).list()
 
-                    rear = conf_RLF[0]  # 1 if Rear , 0 if Front
-                    lower = conf_RLF[1]  # 1 if Lower, 0 if Upper (elbow)
-                    flip = conf_RLF[2]  # 1 if Flip , 0 if Non flip (Flip is usually when Joint 5 is negative)
+                # 2024.01.12 기준으로 if len(all_solutions) == 0: 이 부분으로 진입하지 않음 수정해야함!!
+                if len(all_solutions) == 0:
+                    print("IK unreachable!!")
+                    # 현재는 메시지 출력으로 디버깅하고, 나중에는 단순 변수 변환으로 변경예정(2024.01.12)
+                    #self.rdk.ShowMessage('There is no solution for requested position', True)
+                    self.reachable = False
+                else:
+                    for j in all_solutions:
+                        conf_RLF = self.robot.JointsConfig(j).list()
 
-                    if rear == 0 and lower == 0 and flip == 0:
-                        self.ik_results = j
-                        socket_lock.acquire()
-                        self.robot.setJoints(self.ik_results[:6])
-                        socket_lock.release()
-                        # IK 계산이 완료된 후, position과 rotation을 None으로 설정
-                        self.position = None
-                        self.rotation = None
-                        break
+                        rear = conf_RLF[0]  # 1 if Rear , 0 if Front
+                        lower = conf_RLF[1]  # 1 if Lower, 0 if Upper (elbow)
+                        flip = conf_RLF[2]  # 1 if Flip , 0 if Non flip (Flip is usually when Joint 5 is negative)
+
+                        if rear == 0 and lower == 0 and flip == 0:
+                            self.ik_results = j
+                            socket_lock.acquire()
+                            self.robot.setJoints(self.ik_results[:6])
+                            socket_lock.release()
+                            # IK 계산이 완료된 후, position과 rotation을 None으로 설정
+                            self.position = None
+                            self.rotation = None
+                            self.reachable = True
+                            print("IK reachable!!")
+                            break
+
             time.sleep(0.001)
 
     def start_server(self):
@@ -110,6 +125,7 @@ class WebSocketCommunication:
             socket_lock.acquire()
             current_joints = self.robot.Joints()
             current_tables = self.turntable.Joints()
+            reachable = self.reachable
             socket_lock.release()
             # 로봇 관절 각도 값들이나 턴테이블의 각도값이 변하였을 때 데이터 전송
             if not np.array_equal(current_joints, self.previous_joints):
@@ -136,6 +152,9 @@ class WebSocketCommunication:
                 json_data2 = json.dumps(data2)
                 await websocket.send(json_data2)
                 self.previous_tables = current_tables
+
+            json_data3 = json.dumps(reachable)
+            await  websocket.send(json_data3)
 
             # send_joint_positions에서 webSocket을 독점하는 것을 막기 위해
             await asyncio.sleep(0.00001)
