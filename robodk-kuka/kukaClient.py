e9d3f1b602a4092b46f86349c55b888c7ffcb4bf
Jaehong Yoo <143786746+galy96@users.noreply.github.com>
2024-03-08 11:14:58 +0900

    2024.03.08 - 미러링 코드 추가 kukaClient.py: 서버로부터 데이터 받아오는 모듈 robodk_kuka_server_v2.py: kukaClient.py 이용추가

---
+++ robodk-kuka/kukaClient.py
@@ -0,0 +1,114 @@
+import sys
+import struct
+import random
+import socket
+import time
+
+
+class kukaClient:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.msg_id = random.randint(1, 100)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.sock.connect((self.ip, self.port))
+        except socket.error as e:
+            print(f"Socket error: {e}")
+            sys.exit(-1)
+
+    def test_connection(self):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ret = sock.connect_ex((self.ip, self.port))
+            sock.close()
+            return ret == 0
+        except socket.error as e:
+            print(f"Socket error: {e}")
+            return False
+
+    @property
+    def can_connect(self):
+        return self.test_connection()
+
+    def read(self, var, debug=True):
+        if not isinstance(var, str):
+            raise TypeError('Var name must be a string')
+        self.varname = var.encode('utf-8')
+        return self._read_var(debug)
+
+    def _send_req(self, req):
+        self.rsp = None
+        self.sock.sendall(req)
+        self.rsp = self.sock.recv(256)
+
+    def _pack_read_req(self):
+        req_len = len(self.varname) + 3
+        return struct.pack('!HHBH' + str(len(self.varname)) + 's',
+                           self.msg_id,
+                           req_len,
+                           0,
+                           len(self.varname),
+                           self.varname)
+
+    def _read_var(self, debug):
+        # 패킷 생성
+        req = self._pack_read_req()
+        # 패킷 전송
+        self._send_req(req)
+        return self._read_rsp(debug)
+
+    def _read_rsp(self, debug=False):
+        if self.rsp is None: return None
+        header_format = '!HHBH'
+        header_size = struct.calcsize(header_format)
+        body = self.rsp[header_size:-3]  # Exclude the 3-byte end marker
+        is_ok = self.rsp[-3:]
+        if debug:
+            print('Response:', self.rsp)
+        if is_ok.endswith(b'\x01'):
+            self.msg_id = (self.msg_id + 1) % 65536
+            return body.decode('utf-8')
+
+    def loop_read_var(self, var, interval=1):
+        try:
+            while True:
+                response = self.read(var, debug=False)
+                if response is not None:
+                    print(f"서버로부터 받은 응답: {response}")
+                time.sleep(interval)  # 다음 요청 전에 지연
+        except KeyboardInterrupt:
+            print("지속적인 읽기 중지.")
+
+    def close(self):
+        self.sock.close()
+
+
+def main():
+    ip_add = "192.168.1.129"
+    port = 9000
+
+    client = kukaClient(ip_add, port)
+
+    if not client.can_connect:
+        print("서버에 연결할 수 없습니다.")
+        sys.exit(-1)
+
+    var_name1 = "$OV_PRO"
+    var_name2 = "AXIS_ACT"
+    var_name3 = "AXIS_ACT_MEAS"
+    print(f"변수 '{var_name1}' 에 대한 요청을 보냅니다.")
+    print(f"변수 '{var_name2}' 에 대한 요청을 보냅니다.")
+    print(f"변수 '{var_name3}' 에 대한 요청을 보냅니다.")
+    response1 = client.read(var_name1)
+    response2 = client.read(var_name2)
+    response3 = client.read(var_name3)
+    print(f"서버로부터 받은 응답: {response1}")
+    print(f"서버로부터 받은 응답: {response2}")
+    print(f"서버로부터 받은 응답: {response3}")
+
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
--- robodk-kuka/robodk_kuka_server_v2.py
+++ robodk-kuka/robodk_kuka_server_v2.py
@@ -4,7 +4,23 @@ from robodk.robolink import *
 from robodk_config import Config
 import asyncio
 from asyncio import Event
+from kukaClient import  kukaClient
 
+async def update_robot_angles(robot, stop_event):
+    kuka_client = kukaClient(Config.HOST, Config.PORT)
+    if not kuka_client.can_connect:
+        print("KUKA 서버에 연결할 수 없습니다.")
+        return
+    try:
+        while not stop_event.is_set():
+            response = kuka_client.read("AXIS_ACT", debug=False)
+            if response:
+                print(f"서버로부터 받은 관절각도: {response}")
+                angles = [float(angle) for angle in response.split(',')]  # 응답을 파싱하여 각도 리스트로 변환
+                robot.setJoints(angles)  # 로봇의 관절각도를 설정
+            await asyncio.sleep(0.1)  # 비동기 대기
+    finally:
+        kuka_client.close()  # 클라이언트 연결 종료
 
 async def main():
     # 로봇 연결
@@ -21,19 +37,24 @@ async def main():
 
     # 인스턴스 생성 및 초기화
     stop_event = Event()
+
     ws_comm = WebSocketCommunication(Config.HOST, Config.PORT, robot, tool, turntable, RDK)
 
     # Connect to the robot using default connetion parameters
     success = robot.Connect()
+    print("성공할 경우: True, 실패할 경우: False")
     print(success)
     status, status_msg = robot.ConnectedState()
+    print("ConnectedState: ")
     print(status)
     print(status_msg)
     if status != ROBOTCOM_READY:
         # Stop if the connection did not succeed
         raise Exception("Failed to connect: " + status_msg)
     # Set to run the robot commands on the robot
+    print("로봇과의 연결이 성공적으로 이루어졌습니다. RUNMODE_RUN_ROBOT을 설정합니다.")
     RDK.setRunMode(RUNMODE_RUN_ROBOT)
+
 
     # 초기 설정
     robot.setPoseFrame(reference)
@@ -42,28 +63,14 @@ async def main():
 
     # 웹소켓 서버 태스크 생성
     ws_server_task = asyncio.create_task(ws_comm.start_server())
-    robot_control_task = asyncio.create_task(send2kuka(robot, stop_event))
+    #robot_control_task = asyncio.create_task(send2kuka(robot, stop_event))
+    robot_update_task = asyncio.create_task(update_robot_angles(robot, stop_event))
 
     # 서버 및 로봇 제어 태스크를 기다림
-    await asyncio.gather(ws_server_task, robot_control_task)
+    await asyncio.gather(ws_server_task,
+                         #robot_control_task,
+                         robot_update_task)
 
 # 웹소켓 통신 모듈 인스턴스 생성 및 서버 시작
 if __name__ == "__main__":
     asyncio.run(main())
-
-    # # 로봇 제어 루프
-    # while True:
-    #     joints = robot.Joints()
-    #     print("Current joint values are:", joints)
-    #
-    #     robot.MoveJ(joints)
-    #     print("Robot MoveJ")
-    #     time.sleep(1)
-    #
-    #     print("Robot...")
-    #
-    #     # 무한 루프를 방지하기 위한 대기 시간
-    #     time.sleep(1)
-    #
-    #     if isStop:
-    #         break
